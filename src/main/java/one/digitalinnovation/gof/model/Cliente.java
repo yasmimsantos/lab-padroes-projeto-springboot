c78d1047f4870a942d60705216c9f5f5d7cc5596
yasmimsantos <yasmimsendensantos@gmail.com>
2023-02-22 18:07:19 -0300

    Upload inicial via Intellij.

---
+++ src/main/java/one/digitalinnovation/gof/model/Cliente.java
@@ -0,0 +1,48 @@
+package one.digitalinnovation.gof.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Cliente {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String nome;
+	private String produto;
+	@ManyToOne
+	private Endereco endereco;
+
+	public Cliente(String produto) {
+		this.produto = produto;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+}
